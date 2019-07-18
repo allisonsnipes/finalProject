a0d35125aaf5f92d4c8fdd7fdedbb5af061c14df
Allison Snipes <ajsnipes@usfca.edu>
2019-07-17 17:26:51 -0700

    setting up files

---
+++ finalproject/src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function footer() {
+	return (
+		<footer className="footer">
+			<span> Game Friend 2019</span>
+		</footer>
+	);
+}
+
+export default footer;
