c2ad95743ec0b3a516c787d406ba5c924719b630
Allison Snipes <ajsnipes@usfca.edu>
2019-07-17 19:54:39 -0700

    done

--- finalproject/src/pages/homePage.js
+++ finalproject/src/pages/homePage.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import footer from '../components/footer';
-import jumbotron from '../components/jumbotron';
-import navbar from '../components/navbar';
-import pictures from '../components/pictures';
-import createAccountModal from '../components/createAccountModal';
-import loginModal from '../components/loginModal';
-import wrapper from '../components/wrapper';
-import form from '../components/form';
-import col from '../components/col';
-import Footer from '../components/footer';
+import React from 'react'
+import footer from '../components/footer'
+import jumbotron from '../components/jumbotron'
+import navbar from '../components/navbar'
+import pictures from '../components/pictures'
+import createAccountModal from '../components/createAccountModal'
+import loginModal from '../components/loginModal'
+import wrapper from '../components/wrapper'
+import form from '../components/form'
+import col from '../components/col'
+import Footer from '../components/footer'
 
-function index() {
-	return (
-		<div>
-			<navbar />
+function index () {
+  return (
+    <div>
+      <Navbar />
 
-			<container>
-				<row>
-					<Col size="md-12">
-						<Jumbotron>
-						</Jumbotron>
-					</Col>
-				</row>
-
-				<pictures />
-			</container>
-		</div>
-		<Footer></Footer>
-	);
+      <Container>
+        <Row>
+          <Col size='md-12'>
+            <Jumbotron />
+          </Col>
+        </Row>
+        <Pictures />>
+      </Container>
+      <Footer />
+    </div>
+  )
 }
