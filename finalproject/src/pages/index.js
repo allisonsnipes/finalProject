fcdede3189dc7a3f3fc6df627c07816880dacd43
Allison Snipes <ajsnipes@usfca.edu>
2019-07-17 17:47:07 -0700

    adding more

--- finalproject/src/pages/index.js
+++ finalproject/src/pages/index.js
@@ -1,2 +1,30 @@
 import React from "react";
 import footer "../components/footer"
+import jumbotron "../components/jumbotron"
+import navbar "../components/navbar"
+import pictures "../components/pictures"
+import modal "../components/modal"
+import wrapper "../components/wrapper"
+import form "../components/form"
+
+function index() {
+  return(
+    <div>
+      <navbar>
+      </navbar>
+
+      <container>
+        <row>
+          <col>
+
+          </col>
+        </row>
+
+        <pictures></pictures>
+      </container>
+    </div>
+
+    <footer></footer>
+  )
+}
+
