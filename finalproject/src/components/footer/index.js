cc7f9576615110affe4d8c90d08ac7e20bae102f
Allison Snipes <ajsnipes@usfca.edu>
2019-07-19 16:53:32 -0700

    refactor

--- finalproject/src/components/footer/index.js
+++ finalproject/src/components/footer/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 // import './style.css'
 
-function Footer() {
-	return (
-		<Footer className="footer">
-			<span>Game Meet 2017</span>
-		</Footer>
-	);
+function Footer () {
+  return (
+    <Footer className='footer'>
+      <span>Game Meet 2017</span>
+    </Footer>
+  )
 }
 
-export default Footer;
+export default Footer
--- finalproject/src/pages/home.js
+++ finalproject/src/pages/home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Footer from '../components/Footer/index';
 import Jumbotron from '../components/Jumbotron/index';
 import Navbar from '../components/Navbar/index';
-// import Pictures from '../components/pictures'
 import Wrapper from '../components/Wrapper/index';
-// import createAccountModal from '../components/createAccountModal'
-// import LoginModal from '../components/loginModal'
-// import Form from '../components/form'
 import Col from '../components/Col/index';
 import Row from '../components/Row/index';
 import Container from '../components/Container/index';
+// import Pictures from '../components/pictures'
+// import createAccountModal from '../components/createAccountModal'
+// import LoginModal from '../components/loginModal'
+// import Form from '../components/form'
 
 function Home() {
 	return (
@@ -37,7 +37,7 @@ function Home() {
 
 			<Row>
 				<Col size="md-12">
-					<Footer className="footer fixed-bottom text-white" />
+					<Footer className="footer fixed-bottom text-white bg-dark" />
 				</Col>
 			</Row>
 		</Container>
